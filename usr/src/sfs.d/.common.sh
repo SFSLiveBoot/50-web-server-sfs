b137d19089651474322937d525a75205e34535a4
Lauri Korts-Pärn <lauri@cyberdefense.jp>
2017-04-06 11:37:19 -0400

    use latest nginx/nodejs available

--- usr/src/sfs.d/.common.sh
+++ usr/src/sfs.d/.common.sh
@@ -5,6 +5,20 @@
 
 test -n "${SILENT_EXIT+set}" || SILENT_EXIT=1
 
-latest_nodejs() {
-  curl -s https://nodejs.org/dist/latest/ | grep -o "node-v[^-]*-linux-x64.tar.xz" | head -1
+: ${nginx_dl_url:=https://nginx.org/en/download.html}
+: ${nodejs_dist:=https://nodejs.org/dist}
+: ${nodejs_dist_json:=$nodejs_dist/index.json}
+: ${nodejs_arch:=linux-x64}
+
+latest_nodejs_ver() {
+  curl -s "$nodejs_dist_json" | jq -r 'map(select(.lts!=false and contains({files:["'$nodejs_arch'"]})))[0].version'
+}
+
+latest_nodejs_url() {
+  : ${nodejs_ver:=$(latest_nodejs_ver)}
+  echo "$nodejs_dist/$nodejs_ver/node-${nodejs_ver}-$nodejs_arch.tar.xz"
+}
+
+latest_nginx() {
+  curl -s "$nginx_dl_url" | grep -Eo 'nginx-[0-9][targz.0-9]+' | head -1
 }
