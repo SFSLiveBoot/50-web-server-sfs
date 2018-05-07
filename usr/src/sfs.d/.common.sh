ff24ac4407c98f10f9539461a1e55f9ec6e94936
Lauri Korts-Pärn <lauri@cyberdefense.jp>
2018-05-07 16:13:37 -0400

    add update check with .check-up-to-date

--- usr/src/sfs.d/.common.sh
+++ usr/src/sfs.d/.common.sh
@@ -14,6 +14,10 @@ latest_nodejs_ver() {
   curl -s "$nodejs_dist_json" | jq -r 'map(select(.lts!=false and contains({files:["'$nodejs_arch'"]})))[0].version'
 }
 
+installed_nodejs_ver() {
+  "$DESTDIR/opt/bin/node" --version
+}
+
 latest_nodejs_url() {
   : ${nodejs_ver:=$(latest_nodejs_ver)}
   echo "$nodejs_dist/$nodejs_ver/node-${nodejs_ver}-$nodejs_arch.tar.xz"
@@ -21,4 +25,20 @@ latest_nodejs_url() {
 
 latest_nginx() {
   curl -s "$nginx_dl_url" | grep -Eo 'nginx-[0-9][targz.0-9]+' | head -1
+}
+
+latest_nginx_ver() {
+  latest_nginx | sed -e 's/^nginx-//' -e 's/[targz.]\+$//'
+}
+
+installed_nginx_ver() {
+  "$DESTDIR/srv/nginx/sbin/nginx" -v 2>&1 | sed -e 's^.*/^^'
+}
+
+confirm_latest() {
+  local name="$1" installed="$2" latest="$3"
+  test "x$installed" = "x$latest" || {
+    echo "Update for $name available: '$installed' -> '$latest'" >&2
+    return 1
+  }
 }
